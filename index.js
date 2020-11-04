9cb4b9fe7890ed8f25df1a3e72a586064405bd7b
ZaQuarius15 <jzcarlson014@gmail.com>
2020-11-04 07:12:40 -0600

    commit tree 6d505d59fb8fa1aab640634fd0e4f431d9a42491

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(array, string) {
+    return array.filter(e => 
+        e.toLowerCase() === string.toLowerCase()
+    )
+}
+
+function fuzzyMatch(array, string) {
+    return array.filter(e =>
+        e.toLowerCase().indexOf(string.toLowerCase()) === 0
+    )
+}
+
+function matchName(array, string) {
+    return array.filter(e => 
+        e.name.toLowerCase() === string.toLowerCase()
+    )
+}
